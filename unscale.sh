7453f109e848faa9ebc93be60c2155d2f1b0021f
Ivailo Monev <xakepa10@gmail.com>
2014-11-29 12:25:56 +0000

    move all icons to scalable and add a script to unscale them

---
+++ unscale.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+for size in 16 24 32 48 64 128;do
+    rm -vrf "${size}x${size}/"
+    for file in scalable/*/*.svg;do
+        dir="$(basename $(dirname $file))"
+        base="$(basename $file .svg)"
+        mkdir -vp "${size}x${size}/$dir"
+        rsvg-convert --height="${size}" --width="${size}" "$file" \
+            --format=png --output "${size}x${size}/$dir/$base.png"
+    done
+done
